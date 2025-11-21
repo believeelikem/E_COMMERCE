91b53f58fa1f39598ce812741594f9559d1254b5
Believe Elikem <ayamwenzdayz@gmail.com>
2025-11-21 15:55:58 +0000

    added checkout functionality

--- store/views.py
+++ store/views.py
@@ -14,20 +14,32 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer)
         
-        items = order.orders.all()
+        items = order.items.all()
     else:
         items = []
+        order = {"cart_total_items":0,"card_total_price":0}
     
     context = {
-        "items":items
+        "items":items,
+        "cart_total_price" : order.cart_total,
+        "cart_total_items":order.cart_total_items
     }
     
     return render(request,"store/cart.html",context)
 
 def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer)
+        
+        items = order.items.all()
+    else:
+        items = []
+        order = {"cart_total_items":0,"card_total":0}
     
     context = {
-        
-    }
-    
+        "items":items,
+        "cart_total_price" : order.cart_total,
+        "cart_total_items":order.cart_total_items
+    }    
     return render(request,"store/checkout.html",context)
