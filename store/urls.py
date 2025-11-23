721c1c86c5718c5dfbe3400bdee6ca78e9c72864
Believe Elikem <ayamwenzdayz@gmail.com>
2025-11-23 23:32:20 +0000

    added process order path

--- store/urls.py
+++ store/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("checkout/",views.checkout,name = "checkout"),
     path("update_item/",views.update_item,name = "update_item"),
     path("update-count/",views.update_count,name = "update_count"),
+    path('process_order/', views.processOrder, name="process_order"),
 ]
